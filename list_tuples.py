58905f8487be8738e5b4c9225d5f4d7ddb03916e
https://github.com/nurseincode <nurseincode@gmail.com>
2025-09-14 12:51:57 +1000

    Completed common list methods

--- list_tuples.py
+++ list_tuples.py
@@ -1,7 +1,31 @@
-#  List - Array - Ordered collection of values
-numbers = [13, 2, 23, 53]
-print(numbers)
-print(numbers[3]) # Index 3
+# # -->> List - Array - Ordered collection of values
 
-numbers[2] = 17 # Changes index 2 value
+numbers = [13, 2, 23, 53, 13]
+# # print(numbers[3]) # Index 3 
+# # print(numbers[-1]) # Negative indexing 
+
+# # numbers[2] = 17 # Changes index 2 value
+# # print(numbers)
+
+# # numbers[5] = 72 # Index cannot be accessed: out of range
+# # print(numbers)
+
+# # numbers.append(42) # Adding a value using a method, single values only
+# # print(numbers)
+
+# # numbers.insert(3, 20) # Insert 20 before index 3
+# # print(numbers)
+
+# numbers.pop() # Remove and return last index(default last)
+# popped_value = numbers.pop(2)
+# print(numbers)
+# print(popped_value)
+
+# numbers.remove(13)
+# print(numbers)
+
+# numbers.sort() # Default ascending
+numbers.sort(reverse=True) # Keyword argument
 print(numbers)
+
+print(numbers.count(13)) # Occurences of 13
